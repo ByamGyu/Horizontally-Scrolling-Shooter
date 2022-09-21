2f1094c5fa1e0e9519722f7536e24e14464ff4f6
LeeJangGyu <decjang@naver.com>
2022-09-21 14:12:46 +0900

    # 2022-09-21(수) 탄환 작업

---
+++ Assets/My Scripts/PlayerController.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 플레이어 능력치
+    [SerializeField]
+    float _speed = 5.0f; // 이동 속도 변수
+    [SerializeField]
+    float _Bullet_Shot_Delay_Cur = 0.0f;
+    [SerializeField]
+    float _Bullet_Shot_Delay_Max = 0.2f;
+    [SerializeField]
+    int power = 1;
+
+    // 플레이어 이동 판정
+    [SerializeField]
+    bool _isTouchTop = false;
+    [SerializeField]
+    bool _isTouchBottom = false;
+    [SerializeField]
+    bool _isTouchRight = false;
+    [SerializeField]
+    bool _isTouchLeft = false;
+
+    // 플레이어 애니메이션?
+    [SerializeField]
+    Animator _anim;
+
+    // 오브젝트 저장 변수
+    [SerializeField]
+    public GameObject _Bullet1; // 필요한 오브젝트를 붙일 수 있다. (카메라, 물체 등등)
+    [SerializeField]
+    public GameObject _Bullet2;
+    [SerializeField]
+    public GameObject _Bullet3;
+    [SerializeField]
+    public GameObject _Bullet4;
+    [SerializeField]
+    public GameObject _Bullet5;
+    [SerializeField]
+    public GameObject _Bullet6;
+    [SerializeField]
+    public GameObject _Bullet7;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Move();
+        Fire_Bullet();
+        Bullet_Delay();
+    }
+
+    void Move()
+    {
+        // Input.GetAxisRaw()는 방향 값을 추출하는 함수
+        float _height = Input.GetAxisRaw("Horizontal"); // 키보드 수평 이동 입력 받기
+        float _vertical = Input.GetAxisRaw("Vertical"); // 키보드 수직 이동 입력 받기
+
+        // 경계선 이동 제한하기
+        if ((_isTouchTop == true && _vertical == 1) || (_isTouchBottom == true && _vertical == -1)) _vertical = 0;
+        if ((_isTouchRight == true && _height == 1) || (_isTouchLeft == true && _height == -1)) _height = 0;
+
+        Vector3 CurPos = transform.position; // 현재 위치
+        Vector3 NextPos = new Vector3(_height, _vertical, 0) * _speed * Time.deltaTime; // 이동하는 거리
+
+        transform.position = CurPos + NextPos; // 다음 위치
+    }
+
+    void Fire_Bullet()
+    {
+        // '['를 누르면 발사
+        if (!Input.GetKey(KeyCode.LeftBracket)) return;
+
+        // 총알 생성 딜레이 시간 판별
+        if (_Bullet_Shot_Delay_Cur < _Bullet_Shot_Delay_Max) return;
+
+        switch(power) // 일반 탄환
+        {
+            case 1: // 파워레벨 1
+                // 프리팹(_Bullet1)을 오브젝트로 생성, 생성 위치, 생성 방향
+                GameObject bulletMid1 = Instantiate(_Bullet1, transform.position + Vector3.right * 0.5f, transform.rotation);
+
+                // 탄환에 힘을 가해 움직이게 한다.
+                Rigidbody2D rigid1 = bulletMid1.GetComponent<Rigidbody2D>();
+                rigid1.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+
+                // 총알 생성 딜레이 시간 초기화
+                _Bullet_Shot_Delay_Cur = 0.0f;
+                break;
+            case 2: // 파워레벨 2
+                GameObject bulletTop2 = Instantiate(_Bullet1, transform.position + Vector3.up * 0.15f + Vector3.right * 0.5f, transform.rotation);
+                GameObject bulletDown2 = Instantiate(_Bullet1, transform.position + Vector3.down * 0.15f + Vector3.right * 0.5f, transform.rotation);
+
+                Rigidbody2D rigidTop2 = bulletTop2.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidBottom2 = bulletDown2.GetComponent<Rigidbody2D>();
+                rigidTop2.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+                rigidBottom2.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+
+                _Bullet_Shot_Delay_Cur = 0.0f;
+                break;
+            case 3: // 파워레벨 3
+                GameObject bulletTop3 = Instantiate(_Bullet1, transform.position + Vector3.up * 0.25f + Vector3.right * 0.5f, transform.rotation);
+                GameObject bulletMid3 = Instantiate(_Bullet1, transform.position + Vector3.right * 0.75f, transform.rotation);
+                GameObject bulletBottom3 = Instantiate(_Bullet1, transform.position + Vector3.down * 0.25f + Vector3.right * 0.5f, transform.rotation);
+                Rigidbody2D rigidTop3 = bulletTop3.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidMid3 = bulletMid3.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidBottom3 = bulletBottom3.GetComponent<Rigidbody2D>();
+
+                rigidTop3.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+                rigidMid3.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+                rigidBottom3.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+
+                _Bullet_Shot_Delay_Cur = 0.0f;
+                break;
+        }
+
+        
+    }
+
+    void Bullet_Delay()
+    {
+        if (_Bullet_Shot_Delay_Cur > 10) return;
+
+        _Bullet_Shot_Delay_Cur += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Border_Top":
+                    _isTouchTop = true;
+                    break;
+                case "Border_Bottom":
+                    _isTouchBottom = true;
+                    break;
+                case "Border_Right":
+                    _isTouchRight = true;
+                    break;
+                case "Border_Left":
+                    _isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Border_Top":
+                    _isTouchTop = false;
+                    break;
+                case "Border_Bottom":
+                    _isTouchBottom = false;
+                    break;
+                case "Border_Right":
+                    _isTouchRight = false;
+                    break;
+                case "Border_Left":
+                    _isTouchLeft = false;
+                    break;
+            }
+        }
+    }
+}

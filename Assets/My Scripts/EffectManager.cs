466d8ca9c3d77b557ca9b0140e7dc39d4598957b
LeeJangGyu <decjang@naver.com>
2022-10-07 01:16:31 +0900

    #2022-10-07 이펙트 매니저 개선

--- Assets/My Scripts/EffectManager.cs
+++ Assets/My Scripts/EffectManager.cs
@@ -14,6 +14,9 @@ public class EffectManager : MonoBehaviour
     [SerializeField]
     GameObject[] _EffectLists;
 
+    // return 전용 변수
+    GameObject _effecttmp;
+
     public Dictionary<string, GameObject> _DicEffectStorage = new Dictionary<string, GameObject>();
 
 
@@ -65,32 +68,6 @@ public class EffectManager : MonoBehaviour
         }
     }
 
-
-    GameObject go;
-    public GameObject GetEffect(string _effectname, Vector3 _pos, Vector3 _transform, Transform _parent = null)
-    {
-        
-
-        if (_DicEffectStorage.ContainsKey(_effectname))
-        {
-            if (_parent != null) // 부모 지정할 객체의 transform을 _parent에 넣어주면 됨
-            {
-                go = Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
-                go.transform.SetParent(_parent);
-            }
-            else
-            {
-                Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
-            }
-        }
-        else
-        {
-            Debug.Log("Effect: " + _effectname + " is not exist");
-        }
-
-        return go;
-    }
-
     public void SpawnEffect(string _effectname, Vector3 _pos, Vector3 _transform, Vector3 _scale, Transform _parent = null)
     {
         if (_DicEffectStorage.ContainsKey(_effectname))
@@ -110,5 +87,51 @@ public class EffectManager : MonoBehaviour
         {
             Debug.Log("Effect: " + _effectname + " is not exist");
         }
+    }
+
+    // 생성된 이펙트를 반환해주는 함수
+    public GameObject GetEffect(string _effectname, Vector3 _pos, Vector3 _transform, Transform _parent = null)
+    {
+        if (_DicEffectStorage.ContainsKey(_effectname))
+        {
+            if (_parent != null) // 부모 지정할 객체의 transform을 _parent에 넣어주면 됨
+            {
+                _effecttmp = Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
+                _effecttmp.transform.SetParent(_parent);
+            }
+            else
+            {
+                Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
+            }
+        }
+        else
+        {
+            Debug.Log("Effect: " + _effectname + " is not exist");
+        }
+
+        return _effecttmp;
+    }
+
+    public GameObject GetEffect(string _effectname, Vector3 _pos, Vector3 _transform, Vector3 _scale, Transform _parent = null)
+    {
+        if (_DicEffectStorage.ContainsKey(_effectname))
+        {
+            if (_parent != null) // 부모 지정할 객체의 transform을 _parent에 넣어주면 됨
+            {
+                _effecttmp = Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
+                _effecttmp.transform.localScale = new Vector3(_scale.x, _scale.y, _scale.z);
+                _effecttmp.transform.SetParent(_parent);
+            }
+            else
+            {
+                Instantiate(_DicEffectStorage[_effectname], _pos, Quaternion.Euler(_transform.x, _transform.y, _transform.z));
+            }
+        }
+        else
+        {
+            Debug.Log("Effect: " + _effectname + " is not exist");
+        }
+
+        return _effecttmp;
     }
 }
